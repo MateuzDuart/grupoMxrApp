33d5c81689da887b8444424805940aaed1149592
MateuzDuart <ltbmateus@gmail.com>
2023-03-13 22:39:53 -0300

    app mxr

---
+++ android/app/src/main/java/com/example/grupomxr/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.grupomxr;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.grey),
+      home: const MyHomePage(title: 'Grupo MRX'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<File> downloadFile(String url, String fileName) async {
+    var response = await http.get(Uri.parse(url));
+    var dir = await getExternalStorageDirectory();
+    File file = File("${dir!.path}/$fileName");
+    await file.writeAsBytes(response.bodyBytes);
+    return file;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title, style: TextStyle(color: Colors.white)),
+          backgroundColor: Color.fromARGB(255, 65, 65, 65),
+        ),
+        body: InkWell(
+          onTap: () {
+            downloadFile(
+                'https://app.paraibahost.com.br/mobile/dl/apk/63a089110c5d1c735c7016a2',
+                'fileName.apk');
+          }, // Handle your callback
+          child: Ink(height: 100, width: 100, color: Colors.red),
+        ));
+  }
+}
